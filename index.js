dff95de2a1b778c078b14366b4f5f72e2b12c04d
EmanueleEVgames <144163006+EmanueleEVgames@users.noreply.github.com>
2024-10-03 19:49:20 +0200

    create strutture e prima implementazione dell'interfaccia (minuto 19:00)

---
+++ index.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded',()=>
+{
+// qui la logica da eseguire quando è stata caricata la pagina
+
+// prendo l'oggetto con id grid
+const grid=document.querySelector('.grid');
+
+const doodler=document.createElement('div');
+
+let isGameover=false;
+let score 
+let doodlerLeftSpace=50
+let doodlerBottomSpace=150
+let platformCount=5
+let platforms=[]
+
+class Platform{
+    constructor(newPlatBottom){
+        this.left=Math.random()*277 ; // 277 è la differenza tra la larghezza dello sfondo e la larghezza della piattaforma
+        this.bottom=newPlatBottom;
+        this.visual=document.createElement('div');
+
+        const visual =this.visual;
+        visual.classList.add('platform');
+        visual.style.left=this.left+'px';
+        visual.style.bottom=this.bottom+'px';
+        grid.appendChild(visual);
+    }
+}
+
+function createPlatforms(){
+
+    for (let i=0;i<platformCount;i++){
+        let platgap=600/platformCount;
+        let newPlatBottom=80+platgap*i;
+        let newPlatform= new Platform(newPlatBottom);
+        platforms.push(newPlatform);
+        }
+        console.log(platforms);
+}
+
+createPlatforms();
+
+function createDoodler(){ 
+    doodler.classList.add('doodler');
+    grid.appendChild(doodler);
+    doodler.style.bottom=doodlerBottomSpace=doodlerLeftSpace+'px';
+}
+
+createDoodler();
+}
+)
